787183aff782b199a5d90a9cb4ab70c1dadd902f
Sergi <sergi.prof@hotmail.com>
2025-02-03 12:42:56 +0100

    feat: initial commit

---
+++ src/main/java/kata/KataCarsAndTolls/KataCarsAndTollsApplication.java
@@ -0,0 +1,13 @@
+package kata.KataCarsAndTolls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataCarsAndTollsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataCarsAndTollsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kata/KataCarsAndTolls/KataCarsAndTollsApplicationTests.java
@@ -0,0 +1,13 @@
+package kata.KataCarsAndTolls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataCarsAndTollsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
